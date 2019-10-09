cf5a35d019479b644731f63e37ceedba6c812fc7
Juan Carlos Migliavacca <jcmigliavacca@gmail.com>
2019-10-08 22:11:36 -0300

    Change username challenge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+class Username extends React.Component {
+  state = { value: "" };
 
+  changeValue(value) {
+    this.setState({ value });
+  }
+
+  render() {
+    const { value } = this.state;
+    return <h1>{value}</h1>;
+  }
+}
+
+
+const App = () =>  {
+  const nameRef = React.useRef(null);
+  const usernameRef = React.useRef(null);
+
+  function clickHandler() {
+    usernameRef.current.changeValue(nameRef.current.value);
+  }
+
+  return (
+    <div>
+      <button onClick={clickHandler}>Change Username</button>
+      <input type="text" ref={nameRef} />
+      <Username ref={usernameRef} />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
